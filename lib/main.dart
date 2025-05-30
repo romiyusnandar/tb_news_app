ef392938a0262fe76b05dc64964017043175ee66
romiyusnandar <yusromi04@gmail.com>
2025-05-31 01:04:11 +0700

    Add splash screen assets and configurations for dark and light modes
    - Introduced new splash screen images for various resolutions and modes.
    - Updated launch_background.xml files for both night and day themes.
    - Configured styles for Android and iOS to support splash screens.
    - Added support for Flutter Native Splash package to manage splash screen display.
    - Updated web index.html to include splash screen styles and images.
    - Created a splash.yaml configuration file for managing splash screen settings.
    - Added new images for the web splash screen in both light and dark modes.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:berita/pages/home_page.dart';
 import 'package:berita/utils/theme/theme.dart';
 import 'package:berita/utils/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
@@ -18,6 +22,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      FlutterNativeSplash.remove();
+    });
+
     return MaterialApp(
       home: const HomePage(),
       theme: Provider.of<ThemeProvider>(context).themeData,
