e48d96105d9aaf00bbc23c010a0575b273583466
romiyusnandar <yusromi04@gmail.com>
2025-06-02 13:27:19 +0700

    Implement onboarding functionality with navigation, skip option, and page indicators; add device utility methods; update theme background color; add iconsax_flutter dependency.

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,18 +1,28 @@
+import 'package:berita/pages/auth/controllers.onboarding/onboarding_controller.dart';
+import 'package:berita/utils/device_util.dart';
 import 'package:berita/utils/helper.dart';
 import 'package:berita/utils/image_strings.dart';
 import 'package:berita/utils/sizes.dart';
 import 'package:berita/utils/text_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:get/instance_manager.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(OnboardingController());
+
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       body: Stack(
         children: [
           PageView(
+            controller: controller.pageController,
+            onPageChanged: controller.updatePageIndicator,
             children: const [
               OnBoarding(
                 image: TImages.onBoardingImage1,
@@ -30,10 +40,83 @@ class OnboardingPage extends StatelessWidget {
                 subtitle: TTexts.onBoardingSubtitle3
               ),
             ],
-          )
+          ),
+          // Skip button
+          OnBoardingSkip(),
+
+          // dot
+          OnBoardingDotNav(),
+
+          // Next button
+          OnBoardingButton()
         ],
       ),
     );
+  }
+}
+
+class OnBoardingButton extends StatelessWidget {
+  const OnBoardingButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: TSizes.defaultSpace,
+      bottom: TDeviceUtils.getBottomNavigationBarHeight(),
+      child: ElevatedButton(
+        onPressed: () => OnboardingController.instance.nextPage(),
+        style: ElevatedButton.styleFrom(
+          shape: CircleBorder(),
+          // backgroundColor: Theme.of(context).colorScheme.primary,
+        ),
+        child: Icon(Iconsax.arrow_right_3),
+      )
+    );
+  }
+}
+
+class OnBoardingDotNav extends StatelessWidget {
+  const OnBoardingDotNav({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = OnboardingController.instance;
+    return Positioned(
+      bottom: TDeviceUtils.getBottomNavigationBarHeight() + 25,
+      left: TSizes.defaultSpace,
+      child:
+        SmoothPageIndicator(
+          controller: controller.pageController,
+          onDotClicked: controller.dotNavigationClicked,
+          count: 3,
+          effect: ExpandingDotsEffect(
+            activeDotColor: Theme.of(context).colorScheme.primary,
+            dotHeight: 6
+          ),
+        )
+    );
+  }
+}
+
+class OnBoardingSkip extends StatelessWidget {
+  const OnBoardingSkip({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: TDeviceUtils.getAppBarHeight(),
+      right: TSizes.defaultSpace,
+      child: TextButton(
+        onPressed: () => OnboardingController.instance.skipPage(),
+        child: Text("Skip", style: TextStyle(color: Theme.of(context).colorScheme.primary,),)
+        ),
+      );
   }
 }
 
